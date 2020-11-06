ceabc3cd0e58d96b9cfeefd4a5ad170f5995860a
lightionight <16332827+lightionight@users.noreply.github.com>
2020-11-06 17:48:50 +0800

    Day_1: Learn brand new input system

---
+++ Assets/CsScript/Input/PlayerInput.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+namespace GAME_TEST_01
+{
+    public class PlayerInput : MonoBehaviour
+    {
+
+    }
+}
+
